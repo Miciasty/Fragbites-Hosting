ccd7277584cced28732472517838c05dbba9d39b
Mateusz Aftanas <108985589+Miciasty@users.noreply.github.com>
2024-03-20 09:22:13 +0100

    Projekt rozpoczęty - 25.07.2019

---
+++ assets/js/ATLAS-439-B.js
@@ -0,0 +1,135 @@
+function ATLAS_439B()
+{
+    document.getElementById("lang").innerHTML = 
+    '<div class="TopBar">' +
+        '<div class="Left">' +
+            '<div class="F-B"></div>' +
+        '</div>' +
+        '<div class="Right">' +
+            '<div class="R-Box">' +
+                '<a href="https://fragbites.pl/hosting/clientarea.php">Panel Klienta</a> |'  +
+                '<a href="https://fragbites.pl/forum/">Forum</a> |' +
+                '<a href="#">Polityka Prywatności</a>' +
+                
+                '<div id="flag">' +
+                '<div id="ID" onclick="ATLAS_439B();"></div>' +
+                '<div id="ID2" onclick="ATLAS_937A();"></div>' +
+                '</div>' +
+            '</div>' +
+        '</div>' +
+        '<div style="clear:both"></div>' +
+    '</div>' +
+    '<div class="MenuBar">' +
+        '<div class="ATLAS-GRID-TOP">' +
+        '<div class="ATL-A"><a href="#"><div class="ATLAS-GRID-SHOVEL1">STRONA GŁÓWNA</div></a></div>' +
+        '<div class="ATL-A"><a href="https://fragbites.pl/hosting/"><div class="ATLAS-GRID-SHOVEL2">HOSTING</div></a></div>' +
+        '<div class="ATL-A"><a href="https://fragbites.pl/forum/"><div class="ATLAS-GRID-SHOVEL3">FORUM</div></a></div>' +
+        '<div class="ATL-A"><a href="https://fragbites.pl/hosting/contact.php"><div class="ATLAS-GRID-SHOVEL4">KONTAKT</div></a></div>' +
+        '</div>' +
+    '</div>' +
+    '<div class="ATLAS-Gllery">' +
+        '<div class="ATLAS-INFO-TOP_TiT">Otwarta paleta serwerów</div>' +
+        '<div class="ATLAS-INFO-TOP_BoT">Zwiększ moc Twoich projektów, dzięki serwerom wysokiej jakości w przystępnej cenie</div>' +
+        '<div class="ATLAS-INFO-BOT-MC">Już od 99.99 PLN netto / m-c</div>' +
+        '<div class="ATLAS-INFO-BOT_BuT">' +
+            '<a href="https://fragbites.pl/hosting/cart.php?gid=1">' +
+                '<div class="ATLAS-INFO-BOT_BuT-CHK">Sprawdź teraz</div>' +
+            '</a>' +
+        '</div>' +
+        '<div class="ATLAS-BOT-INFO">' +
+            '<div class="ATLAS-BOT-B">' +
+                '<div class="ATLAS-BOT-B-TOP">' +
+                    '<div id="CC-1"></div>' +
+                '</div>' +
+                '<div class="ATLAS-BOT-B-LiN"></div>' +
+                '<div class="ATLAS-BOT-B-TiT">Serwery VPS SSD</div>' +
+                '<div class="ATLAS-BOT-B-DeS">Efektywność, ochrona i funkcjonalność do wielu zastosowań</div>' +
+                '<div class="ATLAS-BOT-B-MC">Już od 19.99 PLN / m-c</div>' +
+                '<div class="ATLAS-BOT-B-A"><a href="https://fragbites.pl/hosting/cart.php?gid=1">Sprawdź</a></div>' +
+            '</div>' +
+            '<div class="ATLAS-BOT-B">' +
+                '<div class="ATLAS-BOT-B-TOP">' +
+                    '<div id="CC-2"></div>' +
+                '</div>' +
+                '<div class="ATLAS-BOT-B-LiN"></div>' +
+                '<div class="ATLAS-BOT-B-TiT">Dedykowane Serwery Gier</div>' +
+                '<div class="ATLAS-BOT-B-DeS">Elastyczne i wysoko wydajne zasoby dla profesjonalnych serwerów</div>' +
+                '<div class="ATLAS-BOT-B-MC">Już od 39.99 PLN / m-c</div>' +
+                '<div class="ATLAS-BOT-B-A"><a href="https://fragbites.pl/hosting/cart.php?gid=5">Sprawdź</a></div>' +
+            '</div>' +
+        '</div>' +
+    '</div>' +
+    '<div class="ATLAS-CTN">' +
+        '<div class="ATLAS-CTN-LiN"></div>' +
+        '<div class="ATLAS-CTN-B1A">' +
+            '<div class="ATLAS-CTN-B1A-TiT">Wiele metod płatności</div>' +
+            '<div class="ATLAS-CTN-B1A-BT1">' +
+                '<div class="ATLAS-CTN-BT1-L"></div>' +
+                '<div class="ATLAS-CTN-BT1-R">' +
+                    'U nas z łatwością doładujesz swój portfel najlepszą dla Ciebie metodą.' +
+                    'Udostępniliśmy możliwość doładowania przez najpopularniejsze metody, takie jak PayPal, PaySafeCard, oraz Karty VISA.' +
+                '</div>' +
+            '</div>' +
+        '</div>' +
+    '</div>' +
+    '<div class="ATLAS-BOT">' +
+        '<div class="ATLAS-CTN-LiN-2"></div>' +
+        '<div class="ATLAS-BOT-TiT">Nasza szeroka oferta</div>' +
+        '<div class="ATLAS-BOT-C1A">' +
+            '<div class="ATLAS-BOT-C1A-B">' +
+                '<div id="ABC-B1"></div>' +
+                '<div class="ATLAS-BOT-C1A-B-TiT"><a href="https://fragbites.pl/hosting/cart.php?gid=1">Serwery VPS</a></div>' +
+            '</div>' +
+            '<div class="ATLAS-BOT-C1A-B">' +
+                '<div id="ABC-B3"></div>' +
+                '<div class="ATLAS-BOT-C1A-B-TiT"><a href="https://fragbites.pl/hosting/cart.php?gid=3">Dedykowane</a></div>' +
+            '</div>' +
+            '<div class="ATLAS-BOT-C1A-B">' +
+                '<div id="ABC-B2"></div>' +
+                '<div class="ATLAS-BOT-C1A-B-TiT"><a href="https://fragbites.pl/hosting/cart.php?gid=5">Serwery Minecraft</a></div>' +
+            '</div>' +
+        '</div>' +
+        '<div class="ATLAS-BOT-PlG">' +
+            'Obsługujemy serwery takie jak: 7 Days to Die, ARK: Survival Evolved, Arma 3,'  +
+            'Counter Strike 1.6, Counter Strike Source, Counter Strike Global Offensive, ' +
+            'Factorio, Garrys Mod, Insurgency, Left 4 Dead (2), Minecraft (Bedrock/Java/Pocket Edition), ' +
+            'Rust, Space Engineers, StartBound, Team Fortress 2, The Forest.' +
+        '</div>' +
+    '</div>' +
+    '<div class="ATLAS-FTeR">' +
+        '<div class="ATLAS-FTeR-LiN"></div>' +
+        '<div class="ATLAS-FTeR-B1A">' +
+            '<div class="ATLAS-FTeR-B1A-InF">' +
+                '<div class="ATLAS-FTeR-TiT"><i class="fas fa-bars"></i> NAWIGACJA</div>' +
+                '<div class="ATLAS-FTeR-A"><a href="#"><i class="fas fa-angle-double-right"></i> Strona Główna</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/announcements"><i class="fas fa-angle-double-right"></i> Aktualności</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/cart.php"><i class="fas fa-angle-double-right"></i> Oferta</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/forum/"><i class="fas fa-angle-double-right"></i> Współpraca</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/forum/index.php?/topic/8-wz%C3%B3r-reklama-serwera-nazwa/"><i class="fas fa-angle-double-right"></i> Reklama</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/cart.php"><i class="fas fa-angle-double-right"></i> Serwery</a></div>' +
+            '</div>' +
+            '<div class="ATLAS-FTeR-B1A-InF">' +
+                '<div class="ATLAS-FTeR-TiT"><i class="fas fa-info-circle"></i> FRAGBITES.PL</div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/index.php"><i class="fas fa-angle-double-right"></i> O nas</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/contact.php"><i class="fas fa-angle-double-right"></i> Kontkat</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="#"><i class="fas fa-angle-double-right"></i> Polityka prywatności</a></div>' +
+                '<div class="ATLAS-FTeR-A-FB"></div>' +
+            '</div>' +
+            '<div class="ATLAS-FTeR-B1A-InF">' +
+                '<div class="ATLAS-FTeR-TiT"><i class="fas fa-link"></i> MEDIA</div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://www.facebook.com/FragBitespl/"><i class="fas fa-angle-double-right"></i> Facebook</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://discord.gg/83rgxKq"><i class="fas fa-angle-double-right"></i> Discord</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="#"><i class="fas fa-angle-double-right"></i> Twitter</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/forum/"><i class="fas fa-angle-double-right"></i> Forum</a></div>' +
+            '</div>' +
+            '<div class="ATLAS-FTeR-B1A-InF-EnD">' +
+                '<div class="ATLAS-FTeR-TiT"><i class="fas fa-question-circle"></i> POMOC</div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/knowledgebase"><i class="fas fa-angle-double-right"></i> FAQ</a></div>' +
+                /*'<div class="ATLAS-FTeR-A"><a href="#"><i class="fas fa-angle-double-right"></i> Poradniki</a></div>' +*/
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/forum/index.php?/topic/5-g%C5%82%C3%B3wny-regulamin-forum/"><i class="fas fa-angle-double-right"></i> Regulamin</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/contact.php"><i class="fas fa-angle-double-right"></i> Contact</a></div>' +
+            '</div>' +
+        '</div>' +
+    '</div>' +
+    '<div class="ATLAS-FTeR-C_R">© 2016-2019 FragBites.pl</div>';
+}
---
+++ assets/js/ATLAS-937-A.js
@@ -0,0 +1,135 @@
+function ATLAS_937A()
+{
+    document.getElementById("lang").innerHTML = 
+    '<div class="TopBar">' +
+        '<div class="Left">' +
+            '<div class="F-B"></div>' +
+        '</div>' +
+        '<div class="Right">' +
+            '<div class="R-Box">' +
+                '<a href="https://fragbites.pl/hosting/clientarea.php">Customer Account</a> |'  +
+                '<a href="https://fragbites.pl/forum/">Forum</a> |' +
+                '<a href="#">Privacy Policy</a>' +
+                
+                '<div id="flag">' +
+                '<div id="ID" onclick="ATLAS_439B();"></div>' +
+                '<div id="ID2" onclick="ATLAS_937A();"></div>' +
+                '</div>' +
+            '</div>' +
+        '</div>' +
+        '<div style="clear:both"></div>' +
+    '</div>' +
+    '<div class="MenuBar">' +
+        '<div class="ATLAS-GRID-TOP">' +
+        '<div class="ATL-A"><a href="#"><div class="ATLAS-GRID-SHOVEL1">HOME</div></a></div>' +
+        '<div class="ATL-A"><a href="https://fragbites.pl/hosting/"><div class="ATLAS-GRID-SHOVEL2">HOSTING</div></a></div>' +
+        '<div class="ATL-A"><a href="https://fragbites.pl/forum/"><div class="ATLAS-GRID-SHOVEL3">FORUM</div></a></div>' +
+        '<div class="ATL-A"><a href="https://fragbites.pl/hosting/contact.php"><div class="ATLAS-GRID-SHOVEL4">CONTACT</div></a></div>' +
+        '</div>' +
+    '</div>' +
+    '<div class="ATLAS-Gllery">' +
+        '<div class="ATLAS-INFO-TOP_TiT">Open servers palette</div>' +
+        '<div class="ATLAS-INFO-TOP_BoT">Increase the power of your projects with high-quality servers at an affordable price</div>' +
+        '<div class="ATLAS-INFO-BOT-MC">Starting at 99.99 PLN per month</div>' +
+        '<div class="ATLAS-INFO-BOT_BuT">' +
+            '<a href="https://fragbites.pl/hosting/cart.php?gid=1">' +
+                '<div class="ATLAS-INFO-BOT_BuT-CHK">Get started</div>' +
+            '</a>' +
+        '</div>' +
+        '<div class="ATLAS-BOT-INFO">' +
+            '<div class="ATLAS-BOT-B">' +
+                '<div class="ATLAS-BOT-B-TOP">' +
+                    '<div id="CC-1"></div>' +
+                '</div>' +
+                '<div class="ATLAS-BOT-B-LiN"></div>' +
+                '<div class="ATLAS-BOT-B-TiT">VPS SSD Servers</div>' +
+                '<div class="ATLAS-BOT-B-DeS">Efficiency, protection and functionality for many applications</div>' +
+                '<div class="ATLAS-BOT-B-MC">Starting at 19.99 PLN per month</div>' +
+                '<div class="ATLAS-BOT-B-A"><a href="https://fragbites.pl/hosting/cart.php?gid=1">Get started</a></div>' +
+            '</div>' +
+            '<div class="ATLAS-BOT-B">' +
+                '<div class="ATLAS-BOT-B-TOP">' +
+                    '<div id="CC-2"></div>' +
+                '</div>' +
+                '<div class="ATLAS-BOT-B-LiN"></div>' +
+                '<div class="ATLAS-BOT-B-TiT">Dedicated game servers</div>' +
+                '<div class="ATLAS-BOT-B-DeS">Flexible and highly efficient resources for professional servers</div>' +
+                '<div class="ATLAS-BOT-B-MC">Starting at 39.99 PLN per month</div>' +
+                '<div class="ATLAS-BOT-B-A"><a href="https://fragbites.pl/hosting/cart.php?gid=5">Get started</a></div>' +
+            '</div>' +
+        '</div>' +
+    '</div>' +
+    '<div class="ATLAS-CTN">' +
+        '<div class="ATLAS-CTN-LiN"></div>' +
+        '<div class="ATLAS-CTN-B1A">' +
+            '<div class="ATLAS-CTN-B1A-TiT">Many payment methods</div>' +
+            '<div class="ATLAS-CTN-B1A-BT1">' +
+                '<div class="ATLAS-CTN-BT1-L"></div>' +
+                '<div class="ATLAS-CTN-BT1-R">' +
+                    'With us, you can easily top up your wallet with the best method for you.' +
+                    'We have made available the top-up option using the most popular methods, such as PayPal, PaySafeCard, and VISA Cards.' +
+                '</div>' +
+            '</div>' +
+        '</div>' +
+    '</div>' +
+    '<div class="ATLAS-BOT">' +
+        '<div class="ATLAS-CTN-LiN-2"></div>' +
+        '<div class="ATLAS-BOT-TiT">Our wide offer</div>' +
+        '<div class="ATLAS-BOT-C1A">' +
+            '<div class="ATLAS-BOT-C1A-B">' +
+                '<div id="ABC-B1"></div>' +
+                '<div class="ATLAS-BOT-C1A-B-TiT"><a href="https://fragbites.pl/hosting/cart.php?gid=1">VPS Servers</a></div>' +
+            '</div>' +
+            '<div class="ATLAS-BOT-C1A-B">' +
+                '<div id="ABC-B3"></div>' +
+                '<div class="ATLAS-BOT-C1A-B-TiT"><a href="https://fragbites.pl/hosting/cart.php?gid=3">Dedicated</a></div>' +
+            '</div>' +
+            '<div class="ATLAS-BOT-C1A-B">' +
+                '<div id="ABC-B2"></div>' +
+                '<div class="ATLAS-BOT-C1A-B-TiT"><a href="https://fragbites.pl/hosting/cart.php?gid=5">Minecraft Servers</a></div>' +
+            '</div>' +
+        '</div>' +
+        '<div class="ATLAS-BOT-PlG">' +
+            'We support servers such as: 7 Days to Die, ARK: Survival Evolved, Arma 3,'  +
+            'Counter Strike 1.6, Counter Strike Source, Counter Strike Global Offensive, ' +
+            'Factorio, Garrys Mod, Insurgency, Left 4 Dead (2), Minecraft (Bedrock/Java/Pocket Edition), ' +
+            'Rust, Space Engineers, StartBound, Team Fortress 2, The Forest.' +
+        '</div>' +
+    '</div>' +
+    '<div class="ATLAS-FTeR">' +
+        '<div class="ATLAS-FTeR-LiN"></div>' +
+        '<div class="ATLAS-FTeR-B1A">' +
+            '<div class="ATLAS-FTeR-B1A-InF">' +
+                '<div class="ATLAS-FTeR-TiT"><i class="fas fa-bars"></i> NAVIGATION</div>' +
+                '<div class="ATLAS-FTeR-A"><a href="#"><i class="fas fa-angle-double-right"></i> Home</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/announcements"><i class="fas fa-angle-double-right"></i> News</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/cart.php"><i class="fas fa-angle-double-right"></i> Offer</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/forum/"><i class="fas fa-angle-double-right"></i> Cooperation</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/forum/index.php?/topic/8-wz%C3%B3r-reklama-serwera-nazwa/"><i class="fas fa-angle-double-right"></i> Advertisement</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/cart.php"><i class="fas fa-angle-double-right"></i> Servers</a></div>' +
+            '</div>' +
+            '<div class="ATLAS-FTeR-B1A-InF">' +
+                '<div class="ATLAS-FTeR-TiT"><i class="fas fa-info-circle"></i> FRAGBITES.PL</div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/index.php"><i class="fas fa-angle-double-right"></i> Obout us</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/contact.php"><i class="fas fa-angle-double-right"></i> Contact</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="#"><i class="fas fa-angle-double-right"></i> Privacy policy</a></div>' +
+                '<div class="ATLAS-FTeR-A-FB"></div>' +
+            '</div>' +
+            '<div class="ATLAS-FTeR-B1A-InF">' +
+                '<div class="ATLAS-FTeR-TiT"><i class="fas fa-link"></i> MEDIA</div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://www.facebook.com/FragBitespl/"><i class="fas fa-angle-double-right"></i> Facebook</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://discord.gg/83rgxKq"><i class="fas fa-angle-double-right"></i> Discord</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="#"><i class="fas fa-angle-double-right"></i> Twitter</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/forum/"><i class="fas fa-angle-double-right"></i> Forum</a></div>' +
+            '</div>' +
+            '<div class="ATLAS-FTeR-B1A-InF-EnD">' +
+                '<div class="ATLAS-FTeR-TiT"><i class="fas fa-question-circle"></i> POMOC</div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/knowledgebase"><i class="fas fa-angle-double-right"></i> FAQ</a></div>' +
+                /*'<div class="ATLAS-FTeR-A"><a href="#"><i class="fas fa-angle-double-right"></i> Poradniki</a></div>' +*/
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/forum/index.php?/topic/5-g%C5%82%C3%B3wny-regulamin-forum/"><i class="fas fa-angle-double-right"></i> Regulations</a></div>' +
+                '<div class="ATLAS-FTeR-A"><a href="https://fragbites.pl/hosting/contact.php"><i class="fas fa-angle-double-right"></i> Contact</a></div>' +
+            '</div>' +
+        '</div>' +
+    '</div>' +
+    '<div class="ATLAS-FTeR-C_R">© 2016-2019 FragBites.pl</div>';
+}
